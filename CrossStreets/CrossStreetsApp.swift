325f33ff0099cf99158be242ea016b67bb6171f4
Tyler <tyler@vxlabs.co>
2025-07-11 11:46:43 -0400

    Initial Commit

---
+++ CrossStreets/CrossStreetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossStreetsApp.swift
+//  CrossStreets
+//
+//  Created by Tyler Amos 24 on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CrossStreetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CrossStreetsTests/CrossStreetsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CrossStreetsTests.swift
+//  CrossStreetsTests
+//
+//  Created by Tyler Amos 24 on 7/11/25.
+//
+
+import Testing
+@testable import CrossStreets
+
+struct CrossStreetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
